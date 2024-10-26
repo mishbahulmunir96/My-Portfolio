132a32da90fedbe69f3a4085c8d13534450cc7c4
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-10-26 08:45:32 +0700

    add testimonial section

--- src/components/Testimoni.tsx
+++ src/components/Testimoni.tsx
@@ -1,13 +1,20 @@
+import SubTitle from "./Elements/SubTitle";
+import TestiClient from "./Elements/TestiClient";
 import Title from "./Elements/Title";
 
 const Testimoni = () => {
   return (
     <section className="bg-gray-100">
-      <div className="container m-auto">
+      <div className="container m-auto text-center">
         <Title>
           <span className="text-slate-900">Client</span> Testimonial
         </Title>
-        <h1></h1>
+        <SubTitle>Feedback From Client</SubTitle>
+
+        <div className="grid grid-cols-2 gap-5">
+          <TestiClient />
+          <TestiClient />
+        </div>
       </div>
     </section>
   );
